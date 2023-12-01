7a9d94f1c6cf9e3c11977ad8569bfc30e57f3be4
shubhamkcs <shubham.varneshiya@kcsitglobal.com>
2023-12-01 08:48:35 +0530

    inital commit

---
+++ index.js
@@ -0,0 +1,6 @@
+module.exports = {
+    rules: {
+      'no-console' : require('./rules/no-console'),
+      'max-params' : require('./rules/max-params')
+    },
+  };
---
+++ rules/max-params.js
@@ -0,0 +1,45 @@
+module.exports = {
+    meta: {
+      type: 'suggestion',
+      docs: {
+        description: 'Enforce a maximum number of parameters for functions',
+        category: 'Best Practices',
+        recommended: true,
+      },
+      schema: [
+        {
+          type: 'object',
+          properties: {
+            maxParams: {
+              type: 'integer',
+              minimum: 0,
+            },
+          },
+          additionalProperties: false,
+        },
+      ],
+    },
+    create: function (context) {
+      const { maxParams = 3 } = context.options[0] || {}; // Default maximum parameters to 3 if not provided
+  
+      return {
+        FunctionDeclaration(node) {
+          if (node.params.length > maxParams) {
+            context.report({
+              node,
+              message: `Function has more than ${maxParams} parameters.`,
+            });
+          }
+        },
+        FunctionExpression(node) {
+          if (node.params.length > maxParams) {
+            context.report({
+              node,
+              message: `Function has more than ${maxParams} parameters.`,
+            });
+          }
+        },
+      };
+    },
+  };
+  
